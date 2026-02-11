ba1bfa81e7629f82b8d5877ba3a5ef312f0f2c95
yt2b <reinventing.the.wheels@gmail.com>
2026-02-11 22:43:06 +0900

    feat: キー入力処理を実装

--- src/state.rs
+++ src/state.rs
@@ -2,12 +2,36 @@ use core::Chip8;
 use ggez::{
     event::EventHandler,
     graphics::{Color, DrawMode, Mesh},
+    input::keyboard::KeyCode,
     *,
 };
 
 pub const SIZE: usize = 14;
 pub const SPACE: usize = 2;
 const FRAME_TIME: f32 = 1000.0 / 60.0;
+// Key mapping
+// 1 2 3 C -> 1 2 3 4
+// 4 5 6 D -> Q W E R
+// 7 8 9 E -> A S D F
+// A 0 B F -> Z X C V
+const KEYCODES: [KeyCode; 16] = [
+    KeyCode::X,
+    KeyCode::Key1,
+    KeyCode::Key2,
+    KeyCode::Key3,
+    KeyCode::Q,
+    KeyCode::W,
+    KeyCode::E,
+    KeyCode::A,
+    KeyCode::S,
+    KeyCode::D,
+    KeyCode::Z,
+    KeyCode::C,
+    KeyCode::Key4,
+    KeyCode::R,
+    KeyCode::F,
+    KeyCode::V,
+];
 
 pub struct State {
     chip8: Chip8,
@@ -29,7 +53,18 @@ impl State {
 
 impl EventHandler for State {
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
-        self.chip8.step(0);
+        let key = KEYCODES
+            .iter()
+            .enumerate()
+            .map(|(i, &kc)| {
+                if ctx.keyboard.is_key_pressed(kc) {
+                    1 << i
+                } else {
+                    0
+                }
+            })
+            .sum();
+        self.chip8.step(key);
         if !self.is_first_frame {
             self.elapsed_ms += ctx.time.delta().as_secs_f32() * 1000.0;
             while self.elapsed_ms >= FRAME_TIME {
